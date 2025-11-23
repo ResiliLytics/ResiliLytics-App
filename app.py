bda1d50cf11bcf8d74ee257a5e4633465c285f99
AuraFusion <244192501+AuraFusion@users.noreply.github.com>
2025-11-23 17:25:03 -0600

    Update app.py

--- app.py
+++ app.py
@@ -87,7 +87,7 @@ if uploaded_file:
     st.markdown("---")
     st.dataframe(df.head())
 else:
-    st.markdown(" <div style='background:#228be6; color:#ffffff; font-size:1.1rem; font-weight:600;'>Please upload a file to view dynamic metrics.</div>", unsafe_allow_html=True)
+    st.markdown(" <div style='background:#f6c542; color:#ffffff; font-size:1.1rem; font-weight:600;'>Please upload a file to view dynamic metrics.</div>", unsafe_allow_html=True)
 
 st.markdown("### Dashboard Preview")
 st.image("https://github.com/AuraFusion/supplysight-assets/blob/main/Final%20Dashboard%20Sample.png?raw=true")
