30baf95fc0812a8db71df6cf1f1a0926a57318e4
AuraFusion <244192501+AuraFusion@users.noreply.github.com>
2025-11-23 16:09:28 -0600

    Update app.py

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ st.set_page_config(page_title="SupplySight", layout="wide")
 st.markdown("""
     <div style='text-align: center; padding: 0; margin: 0; line-height: 1;'>
         <img src='https://raw.githubusercontent.com/AuraFusion/supplysight-assets/main/logo.png' alt='SupplySight Logo' width='300' style='display: block; margin: 0 auto -10px auto;'/>
-        <h1 style='color: #ffffff; margin: 0; padding: 0;'>SupplySight Dashboard</h1>
+                <h1 style='color: #ffffff; margin-bottom: .2rem; margin-top: -0.2rem;'>SupplySight Dashboard</h1>
         <h3 style='color: #bbbbbb; font-weight:400; margin: 0; padding: 0;'>AI-powered SME Resilience & Risk</h3>
     </div>
 """, unsafe_allow_html=True)
